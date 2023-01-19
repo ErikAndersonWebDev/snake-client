fc2e7225b8c6cad7482121f19602b098f6ccea2b
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2023-01-18 17:38:44 -0800

    constants.js

--- input.js
+++ input.js
@@ -26,6 +26,9 @@ const handleUserInput = function (key) {
   if (key ===  "d"){
     connection.write("Move: right")
   }
+  if (key === "f") {
+    connection.write("Say: Slither me timbers!")
+  }
 }
 
 module.exports = {
